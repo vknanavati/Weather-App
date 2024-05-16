c6e1753aeb03fa7d4b7ec444e625adff979f383f
vknanavati <vimin.nanavati@gmail.com>
2024-05-16 13:43:36 -0400

    created new div element for each date.

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ $(document).ready(function () {
     document.getElementById("mySubmit").onclick = function () {
         cityName = document.getElementById("myText").value
 
-
         $.ajax({
-            url: `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},CT,USA&units=imperial&appid=${process.env.API_KEY}`,
+            url: `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},CT,USA&units=imperial&appid=${API_KEY}`,
             method: 'get',
             dataType: 'json',
             success: function (data) {
@@ -15,22 +14,15 @@ $(document).ready(function () {
                 const lon = data[0].lon;
                 $("#result-city").html(data[0].name);
 
-
                 $.ajax({
-                    url: `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${process.env.API_KEY}`,
+                    url: `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`,
                     method: 'get',
                     dataType: 'json',
                     success: function (data) {
                         console.log(data);
                         console.log(data.list)
 
-
-                        const infos = data.list
-                        for (const index in infos) {
-                            console.log(infos[index]["dt_txt"])
-                        };
-
-
+                        //current weather displayed in today-box
                         const iconCode0 = data.list[0].weather[0].icon
                         const iconImage0 = document.createElement("img");
                         iconImage0.src = `https://openweathermap.org/img/wn/${iconCode0}@2x.png`;
@@ -38,9 +30,27 @@ $(document).ready(function () {
                         $("#result-description").html(data.list[0].weather[0].description);
                         $("#result-temp").html(data.list[0].main.temp_max + "° F");
 
+                        const infos = data.list
 
+                        // getting dates/icon code from index 1-6 from array of objects from data to put into each forecast-box
+                        const dates_arr = [];
+                        const icon_arr = []
+                        const n = 8;
+                        for (let i = 1; i < n; i++) {
+                            dates_arr.push(infos[i]["dt_txt"]);
+                            icon_arr.push(infos[i]["weather"][0]["icon"])
+                        }
+                        console.log(dates_arr);
+                        console.log(icon_arr);
 
+                        // create new div element for each date, append it to #forecast element
+                        $.each(dates_arr, function (index, value) {
 
+                            $("#forecast").append('<div>' + value + '</div>');
+                        });
+
+                        //date, weather icon, low/high temp, weather description for each forecast-box
+                        //forecast-box 1
                         $("#date1").html(data.list[1].dt_txt);
                         const iconCode = data.list[1].weather[0].icon
                         const iconImage = document.createElement("img");
@@ -51,6 +61,7 @@ $(document).ready(function () {
                         $("#temp1").html(`${tempLo}/${tempHi}`);
                         $("#description1").html(data.list[1].weather[0].description);
 
+                        //forecast-box 2
                         $("#date2").html(data.list[2].dt_txt);
                         const iconCode1 = data.list[2].weather[0].icon;
                         const iconImage1 = document.createElement("img");
